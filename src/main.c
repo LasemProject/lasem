d1db49f3c38f7a87e8594dc12e13d2a93908974c
Emmanuel Pacaud <emmanuel.pacaud@lapp.in2p3.fr>
2007-12-28 15:36:42 +0100

    Initial import.

---
+++ src/main.c
@@ -0,0 +1,28 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * main.c
+ * Copyright (C) Emmanuel Pacaud 2007 <emmanuel@gnome.org>
+ * 
+ * main.c is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * main.c is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with main.c.  If not, write to:
+ * 	The Free Software Foundation, Inc.,
+ * 	51 Franklin Street, Fifth Floor
+ * 	Boston, MA  02110-1301, USA.
+ */
+
+#include <stdio.h>
+int main()
+{
+	printf("Hello world\n");
+	return (0);
+}
