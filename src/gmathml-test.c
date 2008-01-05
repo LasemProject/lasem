6cf85fc8a50dcf5208946129f33265be04a60073
Emmanuel Pacaud <emmanuel@gnome.org>
2008-01-05 22:21:44 +0100

    Use the SAX based loader.

--- src/gmathml-test.c
+++ src/gmathml-test.c
@@ -29,84 +29,46 @@
 #include <gmathmlnumberelement.h>
 #include <gmathmlrowelement.h>
 #include <gmathmlview.h>
+#include <gmathmlparser.h>
 #include <glib/gmessages.h>
 #include <cairo-svg.h>
 
-int main()
+int main(int argc, char **argv)
 {
 	GDomNode *document;
-	GDomNode *math_element;
-	GDomNode *row_element;
-	GDomNode *number_element1;
-	GDomNode *number_element2;
-	GDomNode *text1;
-	GDomNode *text2;
-	GDomNode *text3;
-	GDomNode *number_element3;
-	GDomNode *text4;
-	GDomNode *fraction;
 	GMathmlView *view;
 	cairo_t *cairo;
 	cairo_surface_t *surface;
 
 	g_type_init ();
 
-	g_assert (gmathml_operator_get_attributes ("toto", GMATHML_FORM_PREFIX) == NULL);
+	g_assert (gmathml_operator_get_attributes ("toto", GMATHML_FORM_PREFIX) != NULL);
 	g_assert (gmathml_operator_get_attributes ("<>", GMATHML_FORM_INFIX) != NULL);
 
-	document = gmathml_document_new ();
-	g_assert (GDOM_IS_NODE (document));
-	math_element = gmathml_math_element_new ();
-	g_assert (GDOM_IS_NODE (math_element));
-	row_element = gmathml_row_element_new ();
-	number_element1 = gmathml_number_element_new ();
-	g_assert (GDOM_IS_NODE (number_element1));
-	text1 = gdom_text_new ("toto");
-	g_assert (GDOM_IS_NODE (text1));
-	text2 = gdom_text_new ("laripette");
-	g_assert (GDOM_IS_NODE (text2));
-	number_element2 = gmathml_number_element_new ();
-	g_assert (GDOM_IS_NODE (number_element2));
-	text3 = gdom_text_new ("2");
-	g_assert (GDOM_IS_NODE (text3));
-	fraction = gmathml_fraction_element_new ();
-	g_assert (GDOM_IS_NODE (fraction));
-	number_element3 = gmathml_number_element_new ();
-	g_assert (GDOM_IS_NODE (number_element3));
-	text4 = gdom_text_new ("12.3");
-	g_assert (GDOM_IS_NODE (text4));
+/*        g_message ("math class=%s", gdom_element_get_attribute (GDOM_ELEMENT (math_element), "class"));*/
+/*        gdom_element_set_attribute (GDOM_ELEMENT (math_element), "class", "toto");*/
+/*        g_message ("math class=%s", gdom_element_get_attribute (GDOM_ELEMENT (math_element), "class"));*/
 
-	g_assert (gdom_node_append_child (number_element1, text1) != NULL);
-	g_assert (gdom_node_append_child (number_element1, text2) != NULL);
-	g_assert (gdom_node_append_child (row_element, number_element1) != NULL);
-	g_assert (gdom_node_append_child (number_element2, text3)  != NULL);
-	g_assert (gdom_node_append_child (row_element, number_element2) != NULL);
-	g_assert (gdom_node_append_child (fraction, row_element) != NULL);
-	g_assert (gdom_node_append_child (fraction, number_element3) != NULL);
-	g_assert (gdom_node_append_child (number_element3, text4) != NULL);
-	g_assert (gdom_node_append_child (math_element, fraction) != NULL);
-	g_assert (gdom_node_append_child (document, math_element) != NULL);
+	if (argc > 1) {
+		document = gmathml_document_from_file (argv[1]);
 
-	g_message ("math class=%s", gdom_element_get_attribute (GDOM_ELEMENT (math_element), "class"));
-	gdom_element_set_attribute (GDOM_ELEMENT (math_element), "class", "toto");
-	g_message ("math class=%s", gdom_element_get_attribute (GDOM_ELEMENT (math_element), "class"));
+		gdom_node_dump (document);
 
-	gdom_node_dump (document);
+		view = gmathml_view_new (GMATHML_DOCUMENT (document));
 
-	view = gmathml_view_new (GMATHML_DOCUMENT (document));
+		surface = cairo_svg_surface_create ("gmathml.svg", 200.0, 50.0);
+		cairo = cairo_create (surface);
+		cairo_surface_destroy (surface);
 
-	surface = cairo_svg_surface_create ("gmathml.svg", 200.0, 50.0);
-	cairo = cairo_create (surface);
-	cairo_surface_destroy (surface);
+		cairo_move_to (cairo, 0, 25);
+		gmathml_view_render (view, cairo);
 
-	cairo_move_to (cairo, 0, 25);
-	gmathml_view_render (view, cairo);
+		cairo_destroy (cairo);
 
-	cairo_destroy (cairo);
+		g_object_unref (view);
 
-	g_object_unref (view);
-
-	g_object_unref (document);
+		g_object_unref (document);
+	}
 
 	return (0);
 }
