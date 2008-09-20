9c1769a490c28ef450b292e4ce7828d0adc535e3
Emmanuel Pacaud <emmanuel.pacaud@lapp.in2p3.fr>
2008-09-20 14:56:43 +0200

    Rename gmathmlconvert to gmathmlrender.
    Also use GPL everywhere.

--- src/gmathmltest.c
+++ src/gmathmltest.c
@@ -1,28 +1,22 @@
 /*
- * gmathmltest - Regression test utility for gmathml
+ * Copyright © 2007-2008  Emmanuel Pacaud
  *
- * Copyright © 2008 Emmanuel Pacaud
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * Permission to use, copy, modify, distribute, and sell this software
- * and its documentation for any purpose is hereby granted without
- * fee, provided that the above copyright notice appear in all copies
- * and that both that copyright notice and this permission notice
- * appear in supporting documentation, and that the name of the authors
- * not be used in advertising or publicity pertaining to distribution
- * of the software without specific, written prior permission.
- * The authors make no representations about the suitability of this
- * software for any purpose.  It is provided "as is" without express
- * or implied warranty.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * THE AUTHORS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
- * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
- * NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
- * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
- * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
- * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  *
- * Authors: Emmanuel Pacaud <emmanuel@gnome.org>
+ * Author:
+ * 	Emmanuel Pacaud <emmanuel@gnome.org>
  */
 
 #include "config.h"
