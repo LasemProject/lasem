ad18456cbcd2a498a259eaefa98d77263a5eebb5
Emmanuel Pacaud <emmanuel.pacaud@lapp.in2p3.fr>
2008-12-13 22:13:21 +0100

    [debug] Allow debug messages using GMATHML_DEBUG environment variable.

--- src/gdomdebug.c
+++ src/gdomdebug.c
@@ -1,14 +1,33 @@
 #include <gdomdebug.h>
 #include <glib/gprintf.h>
+#include <stdlib.h>
 
+static gboolean debug_checked = FALSE;
 static gboolean debug_enabled = FALSE;
+
+static gboolean
+_is_debug_enabled ()
+{
+	const char *debug_var;
+
+	if (debug_checked)
+		return debug_enabled;
+
+	debug_var = g_getenv ("GMATHML_DEBUG");
+
+	debug_enabled = debug_var != NULL ? atoi (debug_var) != 0 : FALSE;
+
+	debug_checked = TRUE;
+
+	return debug_enabled;
+}
 
 void
 gdom_debug (char const *format, ...)
 {
 	va_list args;
 
-	if (!debug_enabled)
+	if (!_is_debug_enabled())
 		return;
 
 	va_start (args, format);
@@ -21,4 +40,5 @@ void
 gdom_debug_enable (void)
 {
 	debug_enabled = TRUE;
+	debug_checked = TRUE;
 }
