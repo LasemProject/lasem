71d72d05c0ee6131cc923bed96eb4058d450f4de
Emmanuel Pacaud <emmanuel.pacaud@lapp.in2p3.fr>
2008-12-15 14:46:05 +0100

    [tests] ignore some sample files.

--- src/gmathmlactionelement.c
+++ src/gmathmlactionelement.c
@@ -29,6 +29,36 @@ gmathml_action_element_get_node_name (GDomNode *node)
 	return "maction";
 }
 
+/* GMathmlElement implementation */
+
+static const GMathmlBbox *
+gmathml_action_element_measure (GMathmlElement *self, GMathmlView *view, const GMathmlBbox *bbox)
+{
+	if (GDOM_NODE (self)->first_child != NULL)
+		self->bbox = *gmathml_element_measure (GMATHML_ELEMENT (GDOM_NODE (self)->first_child),
+						       view, bbox);
+	else
+		self->bbox = gmathml_bbox_null;
+
+	return &self->bbox;
+}
+
+static void
+gmathml_action_element_layout (GMathmlElement *self, GMathmlView *view,
+			       double x, double y, const GMathmlBbox *bbox)
+{
+	if (GDOM_NODE (self)->first_child != NULL)
+		gmathml_element_layout (GMATHML_ELEMENT (GDOM_NODE (self)->first_child),
+					view, x, y, bbox);
+}
+
+static void
+gmathml_action_element_render (GMathmlElement *self, GMathmlView *view)
+{
+	if (GDOM_NODE (self)->first_child != NULL)
+		gmathml_element_render (GMATHML_ELEMENT (GDOM_NODE (self)->first_child), view);
+}
+
 /* GMathmlActionElement implementation */
 
 GDomNode *
@@ -45,11 +75,16 @@ gmathml_action_element_init (GMathmlActionElement *element)
 /* GMathmlActionElement class */
 
 static void
-gmathml_action_element_class_init (GMathmlActionElementClass *klass)
+gmathml_action_element_class_init (GMathmlActionElementClass *m_action_class)
 {
-	GDomNodeClass *d_node_class = GDOM_NODE_CLASS (klass);
+	GDomNodeClass *d_node_class = GDOM_NODE_CLASS (m_action_class);
+	GMathmlElementClass *m_element_class = GMATHML_ELEMENT_CLASS (m_action_class);
 
 	d_node_class->get_node_name = gmathml_action_element_get_node_name;
+
+	m_element_class->measure = gmathml_action_element_measure;
+	m_element_class->layout = gmathml_action_element_layout;
+	m_element_class->render = gmathml_action_element_render;
 }
 
 G_DEFINE_TYPE (GMathmlActionElement, gmathml_action_element, GMATHML_TYPE_PRESENTATION_CONTAINER)
