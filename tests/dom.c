1aba73cd2837910262d2d994a68b85a9db6201a7
Emmanuel Pacaud <emmanuel@gnome.org>
2010-11-03 17:46:54 +0100

    tests: first unit test.

--- tests/dom.c
+++ tests/dom.c
@@ -1,4 +1,21 @@
 #include <glib.h>
+#include <lsmdomimplementation.h>
+
+static void
+create_document_test (void)
+{
+	LsmDomDocument *document;
+
+	document = lsm_dom_implementation_create_document ("math");
+	g_assert (LSM_IS_DOM_DOCUMENT (document));
+
+	g_object_unref (document);
+
+	document = lsm_dom_implementation_create_document ("svg");
+	g_assert (LSM_IS_DOM_DOCUMENT (document));
+
+	g_object_unref (document);
+}
 
 int
 main (int argc, char *argv[])
@@ -6,6 +23,8 @@ main (int argc, char *argv[])
 	int i;
 
 	g_test_init (&argc, &argv, NULL);
+
+	g_test_add_func ("/dom/create-document", create_document_test);
 
 	g_type_init ();
 
