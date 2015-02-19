d240eb848c54ecd11b5f788ee145e854e1752cd2
Emmanuel Pacaud <emmanuel@gnome.org>
2015-02-19 10:18:57 +0100

    tests: fix renderer tool name in fuzztest.sh

--- tests/fuzztest.sh
+++ tests/fuzztest.sh
@@ -3,7 +3,7 @@
 while true ; do
 	../tools/fuzzxml -r 0.1 $1 fuzz.xml
 
-	if ../src/lasem-render-0.4 fuzz.xml -o fuzz.png ; then
+	if ../src/lasem-render-0.6 fuzz.xml -o fuzz.png ; then
 		echo "Fuzzed file processed correctly"
 	else
 		echo "Oops ! Crash file detected..."
